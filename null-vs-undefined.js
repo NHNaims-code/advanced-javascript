be5c3fdfbc4e4803d8c5293131f6e06dc2da8cbe
NHNaims-code <mdnimurhasan99@gmail.com>
2020-08-06 10:13:12 -0700

    null vs undefined cleared  for viva exam

---
+++ null-vs-undefined.js
@@ -0,0 +1,21 @@
+// undefined
+let num;
+function add(num1, num2){
+    console.log(num1 , num2);
+    //no return value
+    //return
+}
+add(15); // one parameter has not given.
+
+
+const friends = ['jamal', 'kamal', 'sima'];
+console.log(friends[15]);
+
+
+const family = {
+    name : 'Khan',
+    address : 'Dhaka, Bangladesh'
+}
+
+console.log(family.member);
+//==================================
