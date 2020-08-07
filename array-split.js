6e8b71240a35431dc4aad04d56cb1146b9870f0c
NHNaims-code <mdnimurhasan99@gmail.com>
2020-08-07 08:45:37 -0700

    slice, splice and join for array concept cleared

---
+++ array-split.js
@@ -0,0 +1,16 @@
+const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+// const copy = nums.slice(4, 8);
+// console.log(copy);
+
+// const cut = nums.splice(4, 2);
+// console.log(cut);
+
+// const cutAdd = nums.splice(4, 2, 55, 44);
+// console.log(cutAdd);
+
+// console.log(nums);
+
+const numsString = nums.join("-");
+console.log(numsString);
+console.log(nums);
